26731b7ec654f925cb2dc02c3e7d4979f721409d
OluchiEze <oluchiezeifedikwa@gmail.com>
2024-12-07 15:04:31 +0100

    To handle forms and data
    Created the routes, views, controllers,storeages folder and app.js to create a form that will handle users input, post and delete
    
    Changes:
    - Express and usersRouter were imported in the app.js file and a server running at Port 4000 was created.
    - The controller was imported in the usersRouter file
    - The index.ejs and createUser.ejs

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+// const usersRouter = require('./route/usersRouter');
+
+app.set('view engine', 'ejs')
+app.set(express.urlencoded({extended: true}));
+// app.use(usersRouter);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`server listening at ${PORT}`));
